a8b71daade2b495a85153d0719e98751754cb057
Muhammadjon <muhammadyashnar2003@gmail.com>
2025-02-17 19:31:31 +0500

    INFRA: Upload Project

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+Console.Write("Salom, mening ismim Kesha. Siznikichi?\n>>> ");
+string ism = Console.ReadLine();
+Console.Write("Xush kelibsiz " + ism + ", yoshingiz nechada?\n>>> ");
+int yosh = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Meniki 3 da, yoshimiz farqi {yosh - 3} yosh ekan." );
