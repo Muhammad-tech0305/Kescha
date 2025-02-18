4ad1cb994a8bddc011671748291ef7d541dd30c8
Muhammadjon <muhammadyashnar2003@gmail.com>
2025-02-18 19:41:48 +0500

    Kesha yoshini taqqoslash

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 
+// Kesha (3) bilan yosh farqi
 Console.Write("Salom, mening ismim Kesha. Siznikichi?\n>>> ");
 string ism = Console.ReadLine();
 Console.Write("Xush kelibsiz " + ism + ", yoshingiz nechada?\n>>> ");
 int yosh = Convert.ToInt32(Console.ReadLine());
+// taqqoslash
 Console.WriteLine($"Meniki 3 da, yoshimiz farqi {yosh - 3} yosh ekan." );
+Console.WriteLine($"Siz Keshadan kattami?\n>>> {yosh > 3}");
+Console.WriteLine($"Siz Keshadan kichikmi?\n>>> {yosh < 3}");
+Console.WriteLine($"Siz Kesha bilan tengdoshmi>\n>>> {yosh == 3}");
