3bf7286fd26894793e08685de96e4ee0799a42cf
Muhammadjon <muhammadyashnar2003@gmail.com>
2025-02-25 21:17:44 +0500

    ternary operator

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ string ism = Console.ReadLine();
 Console.Write("Xush kelibsiz " + ism + ", yoshingiz nechada?\n>>> ");
 int yosh = Convert.ToInt32(Console.ReadLine());
 // taqqoslash
-Console.WriteLine($"Meniki 3 da, yoshimiz farqi {yosh - 3} yosh ekan." );
+/* Console.WriteLine($"Meniki 3 da, yoshimiz farqi {yosh - 3} yosh ekan." );
 Console.WriteLine($"Siz Keshadan kattami?\n>>> {yosh > 3}");
 Console.WriteLine($"Siz Keshadan kichikmi?\n>>> {yosh < 3}");
 Console.WriteLine($"Siz Kesha bilan tengdoshmi?\n>>> {yosh == 3}");
@@ -22,5 +22,10 @@ else if (yosh == 3)
 else
 {
     Console.WriteLine("Siz Keshadan katta.");
-}
- 
+} */
+// ternar operatori
+string message = 
+    yosh>=3
+    ?"Siz Keshadan kichik emas."
+    :"Siz Keshadan kichik.";
+Console.WriteLine(message);
